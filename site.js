20d4deca35923e9c1f4f7711ba5b409f38deb6d3
Ms-McDonald <54591897+Ms-McDonald@users.noreply.github.com>
2022-04-11 11:52:26 -0400

    remove unnecessary imageinfo()

--- site.js
+++ site.js
@@ -87,11 +87,7 @@ const vue_app = Vue.createApp({
       textTime(minutes)
 {
       return Math.trunc(minutes / 60) + " hours " + minutes%60 + " minutes";
- },
- imageInfo(){
-
  }
-
 }
 })
 vue_app.mount("#vue_app")
